a80d2307ba36691a2a2b0650da69833c206f3575
Aaron <15360085185>
2016-11-17 10:51:54 +0800

    save

---
+++ demo-simple/fis-conf.js
@@ -0,0 +1,32 @@
+// fis.match('::packager', {
+//   spriter: fis.plugin('csssprites')
+// });
+
+// fis.match('*', {
+//   useHash: false
+// });
+
+// fis.match('*.js', {
+//   optimizer: fis.plugin('uglify-js')
+// });
+
+// fis.match('*.css', {
+//   useSprite: true,
+//   optimizer: fis.plugin('clean-css')
+// });
+
+// fis.match('*.png', {
+//   optimizer: fis.plugin('png-compressor')
+// });
+
+// 配置配置文件，注意，清空所有的配置，只留下以下代码即可。
+
+
+//清除其他配置，只剩下如下配置
+// fis.match('*.{js,css,png}', {
+//     useHash: false
+// });
+
+fis.media('prod').match('*.{css}', {
+    useHash: false
+});
